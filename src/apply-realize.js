1ca2eacbe06fba6387a6981023736e6d063bc9fa
enderjj <chnhongjj@163.com>
2019-01-17 21:44:33 +0800

    fix: apply modify

--- src/apply-realize.js
+++ src/apply-realize.js
@@ -1,13 +1,36 @@
-Function.prototype.apply2 = function (obj) {
+// Function.prototype.apply2 = function (obj) {
+//   var object = obj || window;
+//   var result;
+
+//   object.func = this; // 设置对象属性函数
+
+//   if (arguments.length <= 1) {
+//     result = object.func();
+//   } else {
+//     var arr = arguments[1]; // 用于保存参数
+//     var args = [];
+
+//     for (var i = 0, len = arr.length; i < len; i++) {
+//       args.push('arr[' + i + ']');
+//     }
+
+//     result = eval('object.func(' + args + ')'); // 执行函数
+
+//     delete object.func; // 删除新增属性
+//   }
+
+//   return result;
+// }
+
+Function.prototype.apply2 = function (obj, arr) {
   var object = obj || window;
   var result;
 
   object.func = this; // 设置对象属性函数
 
-  if (arguments.length <= 1) {
+  if (!arr || !arr.length) {
     result = object.func();
   } else {
-    var arr = arguments[1]; // 用于保存参数
     var args = [];
 
     for (var i = 0, len = arr.length; i < len; i++) {
